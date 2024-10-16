d975366100152a9a63e58f90719b75dc3735be0b
Jenny Pieloor <devjenny.official@gmail.com>
2024-10-17 02:54:08 +0800

    Menu: WIP mobile responsiveness.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -22,45 +22,45 @@ const ramenMenu = [
 
 const Menu = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-8">
-      <p className="text-5xl font-semibold">What’s on our Plate</p>
-      <div className="max-w-[48rem] w-full">
+    <div className="w-full flex flex-col justify-center items-center lg:gap-8 gap-5 mt-[-2rem] md:mt-0">
+      <p className="lg:text-5xl md:text-4xl text-5xl font-semibold">What’s on our Plate</p>
+      <div className="lg:max-w-[48rem] md:max-w-[38rem] max-w-[30rem] w-full">
         <p className="text-lg opacity-80">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s
         </p>
       </div>
-      <div className="flex gap-7 mt-4">
-        <div className="px-5 py-2 rounded-full border-2 border-secondary bg-secondary text-white tracking-wide">
-          <p>Ramen</p>
+      <div className="flex lg:gap-7 gap-5 mt-4">
+        <div className="px-5 py-2 rounded-full lg:border-2 border border-secondary bg-secondary text-white tracking-wide">
+          <p className="lg:text-[16px] text-sm">Ramen</p>
         </div>
-        <div className="px-5 py-2 rounded-full border-2 border-primary/40 tracking-wide text-primary/50">
-          <p>Desserts</p>
+        <div className="px-5 py-2 rounded-full lg:border-2 border border-primary/40 tracking-wide text-primary/50">
+          <p className="lg:text-[16px] text-sm">Desserts</p>
         </div>
-        <div className="px-5 py-2 rounded-full border-2 border-primary/40 tracking-wide text-primary/50">
-          <p>Lunch</p>
+        <div className="px-5 py-2 rounded-full lg:border-2 border border-primary/40 tracking-wide text-primary/50">
+          <p className="lg:text-[16px] text-sm">Lunch</p>
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-12 w-full h-full mt-20">
+      <div className="md:flex justify-center items-center md:gap-12 w-full h-full xl:mt-16 mt-10 hidden">
         {ramenMenu.map((item, index) => (
           <div
             key={index}
             className="flex flex-col justify-center items-center w-[320px] gap-4"
           >
-            <div className="flex items-center h-[320px]">
+            <div className="flex items-center lg:h-[320px] h-[210px]">
               <img
                 src={item.image}
                 className={`h-fit object-cover ${
-                  item.imageSize || "w-[295px]"
+                  item.imageSize || "xl:w-[295px] lg:w-[270px] md:w-[190px]"
                 }`}
               />
             </div>
-            <p className="text-secondary text-2xl font-semibold mt-5">
+            <p className="text-secondary text-2xl font-semibold xl:mt-5 mt-3">
               {item.title}
             </p>
-            <p className="max-w-[243px]">{item.description}</p>
+            <p className="lg:max-w-[243px] lg:text-[16px] text-sm">{item.description}</p>
           </div>
         ))}
       </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,7 +5,7 @@ const MainPage = () => {
   return (
     <div
       id="homepage"
-      className="flex flex-col pb-20 gap-28 max-w-[1440px] w-full mx-auto 2xl:px-[75px] xl:px-7 px-6 relative overflow-x-hidden overflow-y-scroll no-scrollbar"
+      className="flex flex-col pb-20 lg:gap-28 md:gap-20 max-w-[1440px] w-full mx-auto 2xl:px-[75px] xl:px-7 px-6 relative overflow-x-hidden overflow-y-scroll no-scrollbar"
     >
       <Hero />
       <Menu />
