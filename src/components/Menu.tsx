f04d342293513c9aaa7e5333adb58a1a2a4280b8
Jenny Pieloor <devjenny.official@gmail.com>
2024-10-17 02:28:51 +0800

    Implemented static UI layout for the Menu page.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,71 @@
+const ramenMenu = [
+  {
+    image: "/images/sample-3.svg",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+  {
+    image: "/images/sample-3.svg",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    imageSize: "320px",
+  },
+  {
+    image: "/images/sample-3.svg",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+  },
+];
 
 const Menu = () => {
   return (
-    <div>
-      <p className='text-[10rem]'>Hello</p>
-      
-    </div>
-  )
-}
+    <div className="w-full flex flex-col justify-center items-center gap-8">
+      <p className="text-5xl font-semibold">What’s on our Plate</p>
+      <div className="max-w-[48rem] w-full">
+        <p className="text-lg opacity-80">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+      </div>
+      <div className="flex gap-7 mt-4">
+        <div className="px-5 py-2 rounded-full border-2 border-secondary bg-secondary text-white tracking-wide">
+          <p>Ramen</p>
+        </div>
+        <div className="px-5 py-2 rounded-full border-2 border-primary/40 tracking-wide text-primary/50">
+          <p>Desserts</p>
+        </div>
+        <div className="px-5 py-2 rounded-full border-2 border-primary/40 tracking-wide text-primary/50">
+          <p>Lunch</p>
+        </div>
+      </div>
 
-export default Menu
+      <div className="flex justify-center items-center gap-12 w-full h-full mt-20">
+        {ramenMenu.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col justify-center items-center w-[320px] gap-4"
+          >
+            <div className="flex items-center h-[320px]">
+              <img
+                src={item.image}
+                className={`h-fit object-cover ${
+                  item.imageSize || "w-[295px]"
+                }`}
+              />
+            </div>
+            <p className="text-secondary text-2xl font-semibold mt-5">
+              {item.title}
+            </p>
+            <p className="max-w-[243px]">{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,9 +5,8 @@ const MainPage = () => {
   return (
     <div
       id="homepage"
-      className="flex flex-col justify-between pb-10 max-w-[1440px] w-full mx-auto 2xl:px-[75px] xl:px-7 px-6 relative overflow-x-hidden overflow-y-scroll no-scrollbar"
+      className="flex flex-col pb-20 gap-28 max-w-[1440px] w-full mx-auto 2xl:px-[75px] xl:px-7 px-6 relative overflow-x-hidden overflow-y-scroll no-scrollbar"
     >
-      
       <Hero />
       <Menu />
     </div>
